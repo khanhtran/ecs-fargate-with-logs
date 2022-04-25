7e7fa5bcabc1242e943184f30d98a1450283ebd6
Khanh Tran <ktran@cas.org>
2022-04-25 17:08:58 -0400

    init

--- bin/ecs-fargate-with-logs.ts
+++ bin/ecs-fargate-with-logs.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { ManagedPolicy, PermissionsBoundary } from '@aws-cdk/aws-iam';
 import { EcsFargateWithLogsStack } from '../lib/ecs-fargate-with-logs-stack';
 
 const app = new cdk.App();
-new EcsFargateWithLogsStack(app, 'EcsFargateWithLogsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const env = {
+  account:'746878319868',
+  region: 'us-east-2'
+}
+const stack = new EcsFargateWithLogsStack(app, 'EcsFargateWithLogsStack', {
+  env: env
 });
+
+
+const boundary = ManagedPolicy.fromManagedPolicyArn(stack, 'boundary', `arn:aws:iam::${env.account}:policy/cas-infrastructure/permission-boundary-policy`)
+PermissionsBoundary.of(stack).apply(boundary)
--- lib/ecs-fargate-with-logs-stack.ts
+++ lib/ecs-fargate-with-logs-stack.ts
@@ -1,15 +1,100 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from '@aws-cdk/core'
+import * as ec2 from '@aws-cdk/aws-ec2'
+import { SecurityGroup, Vpc } from "@aws-cdk/aws-ec2";
+import * as ecs from '@aws-cdk/aws-ecs'
+import * as iam from '@aws-cdk/aws-iam'
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns"
+import { join } from 'path';
+import { AwsLogDriver, FirelensConfigFileType, FirelensLogRouterType, LogDrivers } from '@aws-cdk/aws-ecs';
+import { ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export class EcsFargateWithLogsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // firelens image for extra configurations
+    // DockerImageCode.fromImageAsset(join(__dirname, '../firelens')),
+    // the cluster with task definition
+    const vpc = Vpc.fromLookup(this, "kxt29-vpc", { vpcId: 'vpc-0b163940bddf70a43' })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsFargateWithLogsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    const cluster = new ecs.Cluster(this, "kxt29-Cluster", {
+      vpc: vpc
+    });
+
+
+    // Create a load-balanced Fargate service
+    new ecs.FargateService(this, "kxt29-FargateService", {
+      cluster: cluster, // Required
+      taskDefinition: this.createTaskDefinition()
+    });
+  }
+
+  createTaskDefinition(): ecs.FargateTaskDefinition {
+
+    // define task definition
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
+      taskRole: this.createTaskRole()
+      // volumes : [
+      //   {
+      //     name : "var-log"
+      //   }
+      // ]
+    });
+
+    // the side car container
+    // fargateTaskDefinition.addFirelensLogRouter('log-router', {
+    //   image: ecs.ContainerImage.fromAsset(join(__dirname, '../aws-for-fluent-bit')),
+    //   essential: true,
+    //   firelensConfig: {
+    //     type: FirelensLogRouterType.FLUENTBIT,
+    //     //options: {              
+    //     //configFileType: FirelensConfigFileType.FILE,              
+    //     //configFileValue: '/extra.conf'
+    //     //}
+    //   },
+    //   memoryReservationMiB: 50,
+    //   logging: new AwsLogDriver({ streamPrefix: 'firelens' })
     // });
+
+    // the main container
+    fargateTaskDefinition.addContainer('app', {
+      // Use an image from DockerHub
+      essential: true,
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      containerName: 'app',
+      logging: LogDrivers.firelens({
+        options: {
+          Name: 'cloudwatch',
+          region: 'us-east-2',
+          log_group_name: 'firelens-blog',
+          auto_create_group: 'true',
+          log_stream_prefix: 'app',
+          'log-driver-buffer-limit': '2097152'
+        }
+      })
+    });
+
+    return fargateTaskDefinition
+  }
+
+  createTaskRole(): iam.IRole {
+    return new iam.Role(this, 'Role', {
+      assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
+      roleName: 'cas-ecs-task-role',
+      inlinePolicies: {
+        'task-policy': new iam.PolicyDocument({
+          statements: [new iam.PolicyStatement({
+            actions: [
+              "logs:CreateLogStream",
+              "logs:CreateLogGroup",
+              "logs:DescribeLogStreams",
+              "logs:PutLogEvents"
+            ],
+            effect: iam.Effect.ALLOW,
+            resources: ["*"]
+          })]
+        })
+      }
+    });
   }
 }
