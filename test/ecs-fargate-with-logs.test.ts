e2bfbbbae705e1f5df4da90e2a8cce8af7c88e6e
Khanh Tran <ktran@cas.org>
2022-04-25 09:09:19 -0400

    Initial commit

---
+++ test/ecs-fargate-with-logs.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as EcsFargateWithLogs from '../lib/ecs-fargate-with-logs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-fargate-with-logs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsFargateWithLogs.EcsFargateWithLogsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
